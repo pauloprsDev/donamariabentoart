8e5335999008e552b0a00c5ee3c41d79ab6fb583
pauloprsDev <contato.pauloprs@gmail.com>
2025-03-09 15:16:35 -0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/donamariabentoart/',
   plugins: [react()],
-  server: {
-    port: 3001,
-    open: true,
-    cors: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  base: '/donamariabentoart/', // Replace with your repository name
 })
