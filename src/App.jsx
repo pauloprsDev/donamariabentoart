3f5e94f338b59935aa1048fb36630d08ebb90ef8
pauloprsDev <contato.pauloprs@gmail.com>
2025-03-08 22:34:19 -0300

    Configuração para deploy no GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import React, { Suspense } from 'react'
 import { AnimatePresence } from 'framer-motion'
 
 import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Loading from './components/Loading'
-import ScrollToTop from './components/ScrollToTop'
-import Footer from './components/Footer'
-import ErrorBoundary from './components/ErrorBoundary'
+import Navbar from './components/navbar'
+import Loading from './components/loading'
+import ScrollToTop from './components/scrolltotop'
+import Footer from './components/footer'
+import ErrorBoundary from './components/errorboundary'
 
 // Import components directly
 import Home from './components/Home'
 import Products from './components/products'
-import About from './components/About'
-import Contact from './components/Contact'
+import About from './components/about'
+import Contact from './components/contact'
 
 // AnimatedRoutes component definition
 function AnimatedRoutes() {
