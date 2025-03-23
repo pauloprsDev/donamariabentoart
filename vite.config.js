a0fb5c1cdcd159095379e465a9197ba3930b1f4a
pauloprsDev <contato.pauloprs@gmail.com>
2025-03-23 14:33:46 -0300

    Preparação para deploy - Dona Maria Bento Art

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'deployment-message',
-      buildStart() {
-        console.log('\x1b[36m%s\x1b[0m', 'Enviando mensagem para deploy no GitHub Pages...');
-      },
-    }
-  ],
+  plugins: [react()],
   base: '/donamariabentoart/',
 })
